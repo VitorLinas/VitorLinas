5a3d1ce7c251a30092bb887f0a19074034b864bf
VitorLinas <103148031+VitorLinas@users.noreply.github.com>
2022-08-28 12:16:54 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+let inputNome = document.getElementById("name");
+let inputEmail =  document.getElementById("email");
+let inputMensagem =  document.getElementById("message");
+
+let envio = document.getElementById("envio");
+let red = document.getElementById("red");
+
+
+function enviar(){
+    let nome = inputNome.value;
+    let email = inputEmail.value;
+    let mensagem = inputMensagem.value;
+
+    
+
+    if(nome==''|| email==''||mensagem==''){
+        window.alert("Preencha todas as áreas");
+        envio.value = "";   
+        red.action = "";
+        red.method = "";
+    }
+    else{
+        window.alert("Eviado! Aguarde o retorno.")
+    }
+}
+
+
+
+
+
+let largura = window.innerWidth;
+
+if(largura > 400){
+    VanillaTilt.init(document.querySelectorAll(".card"), {
+        max: 25, speed:400, glare: true, "max-glare":1
+    });
+}
+
+
+
+
