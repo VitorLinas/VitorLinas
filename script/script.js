030679a7e9b9b667a352bd4da1fe4f86774eafc3
Vitor Gabriel Linas <103148031+VitorLinas@users.noreply.github.com>
2022-10-07 16:58:02 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.container_projetos, #sobre_mim, #formulario, #projetos h1, #sistema h1', {
+    duration: 2000,
+    rotate:{x:0, y:10, z: 0} 
+});
+
